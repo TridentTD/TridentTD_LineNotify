7fd75a7e394f14bf1c7864d0d83bcadd095f19c5
TridentTD <prote2000@gmail.com>
2019-06-18 04:45:41 +0700

    Version 3.0.1 ทำความสะอาดบรรทัดที่เกินมา เวลาส่งข้อความ
    Version 3.0.1
    
    เวลาส่งข้อความบน app จะมีบรรทัดเกินมา
    ได้ทำความสะอาดบรรทัดที่เกินมา เวลาส่งข้อความ
    
    (issued from : K' Kik Bunsoog )

--- src/TridentTD_LineNotify.cpp
+++ src/TridentTD_LineNotify.cpp
@@ -3,12 +3,17 @@
 
  TridentTD_LineNotify.cpp - A simple way to send LINE NOTIFY
 
- Version 1.0  03/04/2560 Buddism Era  (2017)
- Version 1.1  15/02/2561 Buddism Era  (2018)
- Version 2.0  17/04/2561 Buddism Era  (2018)  add notifySticker()  and notifyPicure()
- Version 2.1  17/04/2561 Buddism Era  (2018)  clean up code for smaller code
-
-Copyright (c) 2016-2018 TridentTD
+ Version 1.0   03/04/2560 Buddhism Era  (2017)  by TridentTD
+ Version 1.1   15/02/2561 Buddhism Era  (2018)  by TridentTD
+ Version 2.0   17/04/2561 Buddhism Era  (2018)  add notifySticker()  and notifyPicure() by TridentTD
+ Version 2.1   17/04/2561 Buddhism Era  (2018)  clean up code for smaller code  by TridentTD
+ Version 2.2   20/07/2561 Buddhism Era  (2018)  add notify(number) by TridentTD
+ Version 2.3   rename DEBUG_PRINT
+ Version 2.4   06/01/2562 Buddhism Era  (2019)  support 2.3.0, 2.4.0, 2.4.1, 2.4.2, 2.5.0-rc1, 2.5.0-rc2 ...  by TridentTD
+ Version 3.0   10/01/2562 Buddhism Era  (2019)  support send by imageFile and imageData
+ Version 3.0.1 18/06/2562 Buddhism Era  (2019)  cleanup '\n' code message ending when sending message 
+ 
+Copyright (c) 2016-2019 TridentTD
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -30,9 +35,14 @@ SOFTWARE.
 */
 
 #include "TridentTD_LineNotify.h"
+// #include <FS.h>
 
 #if defined(ESP8266)
 #define USER_AGENT     "ESP8266"
+#include <core_version.h>
+#if !defined(ARDUINO_ESP8266_RELEASE_2_3_0) &&  !defined(ARDUINO_ESP8266_RELEASE_2_4_0) && !defined(ARDUINO_ESP8266_RELEASE_2_4_1)
+#include <WiFiClientSecureAxTLS.h>
+#endif
 #elif defined (ESP32)
 #define USER_AGENT     "ESP32"
 #endif
@@ -78,11 +88,35 @@ bool TridentTD_LineNotify::notifyPicture(String message, String picture_url) {
   return _notify(message, 0,0, picture_url);
 }
 
-bool TridentTD_LineNotify::_notify(String message, int StickerPackageID, int StickerID, String picture_url){
+bool TridentTD_LineNotify::notifyPicture(String message, fs::FS &fs, String path){
+  return _notify(message, 0,0, "", fs, path);
+}
+
+bool TridentTD_LineNotify::notifyPicture(fs::FS &fs, String path){
+  return _notify("", 0,0, "", fs, path);
+}
+
+bool TridentTD_LineNotify::notifyPicture(String message, uint8_t* image_data, size_t image_size) {
+  return _notify(message, 0,0, "", SPIFFS, "", image_data, image_size);
+}
+
+bool TridentTD_LineNotify::notifyPicture(uint8_t* image_data, size_t image_size) {
+  return _notify("", 0,0, "", SPIFFS, "", image_data, image_size);
+}
+
+bool TridentTD_LineNotify::_notify(String message, int StickerPackageID, int StickerID, String picture_url, fs::FS &fs, String path, uint8_t* image_data, size_t image_sz){
   if(WiFi.status() != WL_CONNECTED) return false;
   if(_token == "") return false;
-  
+
+#if defined(ESP8266)
+#if !defined(ARDUINO_ESP8266_RELEASE_2_3_0) &&  !defined(ARDUINO_ESP8266_RELEASE_2_4_0) && !defined(ARDUINO_ESP8266_RELEASE_2_4_1)
+  axTLS::WiFiClientSecure _clientSecure;
+#else
   WiFiClientSecure _clientSecure;
+#endif
+#elif defined (ESP32)
+  WiFiClientSecure _clientSecure;
+#endif
 
   if (!_clientSecure.connect("notify-api.line.me", 443)) {
     TD_DEBUG_PRINT("connection LINE failed");
@@ -90,10 +124,19 @@ bool TridentTD_LineNotify::_notify(String message, int StickerPackageID, int Sti
   }
 
   int httpCode = 404;
+  size_t image_size = 0;
+  File image_file;
 
   String boundary = "----TridentTD_LineNotify--";
+
   String body = "--" + boundary + "\r\n";
-        body += "Content-Disposition: form-data; name=\"message\"\r\n\r\n" + message + " \r\n";
+        // body += "Content-Disposition: form-data; name=\"message\"\r\n\r\n" + message + " \r\n";
+        body += "Content-Disposition: form-data; name=\"message\"\r\n\r\n" + message ;
+
+      if( (StickerPackageID > 0 && StickerID > 0) || picture_url != "" ||  path != "" || image_data !=NULL && image_sz > 0 ){
+        body += " \r\n";
+      }
+
       if( StickerPackageID > 0 && StickerID > 0) {
         body += "--" + boundary + "\r\n";
         body += "Content-Disposition: form-data; name=\"stickerPackageId\"\r\n\r\n" + String(StickerPackageID) + "\r\n";
@@ -106,22 +149,76 @@ bool TridentTD_LineNotify::_notify(String message, int StickerPackageID, int Sti
         body += "--" + boundary + "\r\n";
         body += "Content-Disposition: form-data; name=\"imageFullsize\"\r\n\r\n" + picture_url + "\r\n";
       }
-        body += "--" + boundary + "--";
+      if( path != ""){
+        if( fs.exists(path)) {
+          image_file = fs.open(path, "r");
+          if( image_file) {
+            image_size = image_file.size();
+            body += "--" + boundary + "\r\n";
+            body += "Content-Disposition: form-data; name=\"imageFile\"; filename=\"image.jpg\"\r\n"; 
+            body += "Content-Type: image/jpeg\r\n\r\n";
+          }
+        }
+      }else
+      if(image_data !=NULL && image_sz > 0 ){
+        image_size = image_sz;
+        body += "--" + boundary + "\r\n";
+        body += "Content-Disposition: form-data; name=\"imageFile\"; filename=\"image.jpg\"\r\n"; 
+        body += "Content-Type: image/jpeg\r\n\r\n";
+      }
 
-  String req = "POST /api/notify HTTP/1.1\r\n";
-        req += "Host: notify-api.line.me\r\n";
-        req += "Authorization: Bearer " + _token + "\r\n";
-        req += "User-Agent: " + String(USER_AGENT) + "\r\n";
-        req += "Connection: close\r\n";
-        req += "Cache-Control: no-cache\r\n";
-        req += "Content-Length: " + String(body.length()) + "\r\n";
-        req += "Content-Type: multipart/form-data; boundary=" + boundary + "\r\n\r\n";
-        req += body;
+  String body_end = "--" + boundary + "--\r\n";
+  size_t body_length = body.length() + image_size + body_end.length();
+
+  String header = "POST /api/notify HTTP/1.1\r\n";
+        header += "Host: notify-api.line.me\r\n";
+        header += "Authorization: Bearer " + _token + "\r\n";
+        header += "User-Agent: " + String(USER_AGENT) + "\r\n";
+        header += "Connection: close\r\n";
+        header += "Cache-Control: no-cache\r\n";
+        header += "Content-Length: " + String(body_length) + "\r\n";
+        header += "Content-Type: multipart/form-data; boundary=" + boundary + "\r\n\r\n";
 
   bool Success_h = false;
   uint8_t line_try=3;  //เพิ่มหากส่งไม่สำเร็จ จะให้ลงใหม่ 3 หน
   while(!Success_h && line_try-- > 0) {
-    _clientSecure.print(req);
+    _clientSecure.print( header + body);
+    
+    if(image_size>0){
+      size_t BUF_SIZE = 1024;
+      if( image_file ) {
+        uint8_t buf[BUF_SIZE];
+        size_t sz = image_size;
+        while(image_file.available() && sz){
+          memset(buf,0,BUF_SIZE);
+          if( sz>=BUF_SIZE){
+            image_file.read(buf,BUF_SIZE);
+            _clientSecure.write( buf, BUF_SIZE);
+            sz -=BUF_SIZE;
+          }else{
+            image_file.read(buf,sz); 
+            _clientSecure.write( buf, sz);
+            sz = 0;
+          }
+        }
+        image_file.close();
+      }else
+      if( image_data !=NULL) {
+        uint8_t *p = image_data;
+        size_t sz = image_size;
+        while( p != NULL && sz){
+          if( sz>=BUF_SIZE){
+            _clientSecure.write( p, BUF_SIZE);
+            p += BUF_SIZE; sz -=BUF_SIZE; 
+          }else{
+            _clientSecure.write( p, sz);
+            p += sz; sz = 0; 
+          }
+        }
+      }
+    }
+    _clientSecure.print("\r\n" +body_end);
+
     while( _clientSecure.connected() && !_clientSecure.available()) delay(10);
     if( _clientSecure.connected() && _clientSecure.available() ) {
       String resp = _clientSecure.readStringUntil('\n');
--- src/TridentTD_LineNotify.h
+++ src/TridentTD_LineNotify.h
@@ -3,12 +3,17 @@
 
  TridentTD_LineNotify.h - A simple way to send LINE NOTIFY
 
- Version 1.0  03/04/2560 Buddism Era  (2017)
- Version 1.1  15/02/2561 Buddism Era  (2018)
- Version 2.0  17/04/2561 Buddism Era  (2018)  add notifySticker()  and notifyPicure()
- Version 2.1  17/04/2561 Buddism Era  (2018)  clean up code for smaller code
-
-Copyright (c) 2016-2018 TridentTD
+ Version 1.0   03/04/2560 Buddhism Era  (2017)  by TridentTD
+ Version 1.1   15/02/2561 Buddhism Era  (2018)  by TridentTD
+ Version 2.0   17/04/2561 Buddhism Era  (2018)  add notifySticker()  and notifyPicure() by TridentTD
+ Version 2.1   17/04/2561 Buddhism Era  (2018)  clean up code for smaller code  by TridentTD
+ Version 2.2   20/07/2561 Buddhism Era  (2018)  add notify(number) by TridentTD
+ Version 2.3   rename DEBUG_PRINT
+ Version 2.4   06/01/2562 Buddhism Era  (2019)  support 2.3.0, 2.4.0, 2.4.1, 2.4.2, 2.5.0-rc1, 2.5.0-rc2 ...  by TridentTD
+ Version 3.0   10/01/2562 Buddhism Era  (2019)  support send by imageFile and imageData
+ Version 3.0.1 18/06/2562 Buddhism Era  (2019)  cleanup '\n' code message ending when sending message
+ 
+Copyright (c) 2016-2019 TridentTD
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -37,11 +42,13 @@ SOFTWARE.
 #include <Arduino.h>
 #if defined(ESP8266)
   #include <ESP8266WiFi.h>
+  #include <FS.h>
 #elif defined (ESP32)
   #include <WiFi.h>
   #include <WiFiClientSecure.h>
+  #include <FS.h>
+  #include <SPIFFS.h>
 #endif
-
 //#define  LINENOTIFY_DEBUG_MODE
 
 #ifdef LINENOTIFY_DEBUG_MODE
@@ -63,19 +70,35 @@ class TridentTD_LineNotify {
     void    setToken(String token)      { _token = token; }
     void    setToken(const char* token) { _token = token; }
 
+    // LINE notify ด้วย ข้อความ
     bool    notify(const char* message);
-    bool    notify(String message); 
+    bool    notify(String message);
+
+    // LINE notify ด้วย ตัวเลข 
     bool    notify(float value, uint8_t decimal = 2);
     bool    notify(int value);
-    bool	  notifySticker(String message, int StickerPackageID, int StickerID);
+
+    // LINE notify ด้วย 
+    bool    notifySticker(String message, int StickerPackageID, int StickerID);
     bool    notifySticker(int StickerPackageID, int StickerID);
+
+    // ส่ง รูปขึ้น LINE ด้วย url บนรูปบน Internet
     bool    notifyPicture(String message, String picture_url);
     bool    notifyPicture(String picture_url);
 
+    // ส่ง รูปขึ้น LINE ด้วย file รูปภาพ (jpg/png) ที่อยู่บน SD หรือ SPIFFS ของ ESP8266/ESP32
+    bool    notifyPicture(String message, fs::FS &fs, String path);
+    bool    notifyPicture(fs::FS &fs, String path);
+
+    // ส่ง รูปขึ้น LINE ด้วย image data  ESP8266/ESP32
+    bool    notifyPicture(String message, uint8_t* image_data, size_t image_size);
+    bool    notifyPicture(uint8_t* image_data, size_t image_size);
+
   private:
-    float   _version = 2.2;
+    float   _version = 3.0;
     String  _token;
-    bool		_notify(String message, int StickerPackageID=0, int StickerID=0, String picture_url="");
+    // bool		_notify(String message, int StickerPackageID=0, int StickerID=0, String picture_url="");
+    bool        _notify(String message, int StickerPackageID=0, int StickerID=0, String picture_url="", fs::FS &fs=SPIFFS , String path="", uint8_t* image_data=NULL, size_t image_size=0);
 };
 
 extern TridentTD_LineNotify LINE;
