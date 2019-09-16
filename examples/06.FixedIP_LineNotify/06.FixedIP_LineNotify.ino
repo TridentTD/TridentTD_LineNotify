d2dbb502e86c9a22ed83cc7ad1266dee9fe7677b
TridentTD <prote2000@gmail.com>
2019-09-16 17:28:17 +0700

    เพ่ิมตัวอย่าง FixedIP สำหรับ ส่ง Line Notify
    เพ่ิมตัวอย่าง FixedIP สำหรับ ส่ง Line Notify

---
+++ examples/06.FixedIP_LineNotify/06.FixedIP_LineNotify.ino
@@ -0,0 +1,53 @@
+/* 
+ * ไลบรารี่ TridentTD_LineNotify version 3.0.1
+ * ใช้สำหรับ ส่ง แจ้งเตือนไปยัง LINE สำหรับ ESP8266 และ ESP32
+ * สามารถส่งได้ทั้ง ข้อความ , สติกเกอร์ และรูปภาพ(ด้วย url)
+ * -----------------------------------------------------
+ * ให้ save เป็น file ต่างหากก่อนถึงจะส่ง Line Notify ภาษาไทยได้
+ *
+ * ตัวอย่างสำรหับ Fixed IP
+ */
+
+#include <TridentTD_LineNotify.h>
+
+#define SSID        "--------------------"
+#define PASSWORD    "--------------------"
+#define LINE_TOKEN  "--------------------"
+
+IPAddress fixed_ip   = {192,168,43,17};       // กำหนด IP เอง
+IPAddress gateway_ip = {192,168,43,1};        // เลือก Gateway IP ของเครือข่ายที่ออก internet ได้
+IPAddress subnet     = {255,255,255,0};
+
+void setup() {
+  Serial.begin(115200); Serial.println();
+  Serial.println(LINE.getVersion());
+  
+  WiFi.config(fixed_ip, gateway_ip, subnet, gateway_ip);
+  WiFi.begin(SSID, PASSWORD);
+  Serial.printf("WiFi connecting to %s\n",  SSID);
+  while(WiFi.status() != WL_CONNECTED) { Serial.print("."); delay(400); }
+  Serial.printf("\nWiFi connected\nIP : ");
+  Serial.println(WiFi.localIP());  
+
+  // กำหนด Line Token
+  LINE.setToken(LINE_TOKEN);
+
+  // ตัวอย่างส่งข้อความ
+  LINE.notify("อุณหภูมิ เกินกำหนด");
+
+  // ตัวอย่างส่งข้อมูล ตัวเลข
+  LINE.notify(2342);          // จำนวนเต็ม
+  LINE.notify(212.43434,5);   // จำนวนจริง แสดง 5 หลัก
+
+  // เลือก Line Sticker ได้จาก https://devdocs.line.me/files/sticker_list.pdf
+  LINE.notifySticker(3,240);        // ส่ง Line Sticker ด้วย PackageID 3 , StickerID 240
+  LINE.notifySticker("Hello",1,2);  // ส่ง Line Sticker ด้วย PackageID 1 , StickerID 2  พร้อมข้อความ
+
+  // ตัวอย่างส่ง รูปภาพ ด้วย url
+  LINE.notifyPicture("https://preview.ibb.co/j6G51n/capture25610417181915334.png");
+  LINE.notifyPicture("จตุธาตุ","https://www.fotoaparat.cz/storage/pm/09/10/23/670915_a5351.jpg");
+}
+
+void loop() {
+  delay(1);
+}
